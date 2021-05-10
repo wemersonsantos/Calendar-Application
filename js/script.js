102085010ee2a7fa7b8cc951e500c2ddf487ee49
wemersonsantos <wemersonsantoseng@gmail.com>
2021-05-10 20:43:09 -0300

    tentando de outra maneira

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 const date = new Date();/**instaciando objeto */
 const year = date.getFullYear();
-const monthDays = document.querySelector(".days")
+const monthDays = document.querySelector(".days");
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(); /**o ultimo dia do mês */
 const firstdayIndex = date.getDay(); /*Pega de segunda a sexta*/
 const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();/**ultimo dia do mês anterior */
+let m,n;
+
+
+const dMounth = document.querySelectorAll("[data-mounth]");
+
+dMounth.forEach(mounth =>{
+    let x = new Date();
+    x.setMonth(mounth.dataset.mounth);
+
+    n = x.getDay();//!Pegando os dias da semana que terminaram os meses de 2021
+    m = new Date(date.getFullYear(), x.getMonth() + 1, 0).getDate();//!pegando os ultimos dias dos meses de 2021
+ 
+})
 
 
 document.querySelector("#year").innerHTML = year;/**o ano em html */
@@ -11,12 +24,12 @@ document.querySelector("#yearString").innerHTML = date.toDateString();/**data em
 
 let days = "";
 
-for(let x = firstdayIndex; x > 0; x--){
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-    monthDays.innerHTML = days;
+for(let x = n; x > 0; x--){
+    days += `<div class="prev-date">${m - x + 1}</div>`;
+    
 }
 
-for(let i = 1; i <= lastDay; i++){
+for(let i = 1; i <= m; i++){
     days += `<div>${i}</div>`;
     monthDays.innerHTML = days;
 }
